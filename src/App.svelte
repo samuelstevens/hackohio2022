e5e68d16f107fb000efa1d10671d14cdea23953f
Samuel Stevens <samuel.robert.stevens@gmail.com>
2022-10-08 15:26:36 -0400

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,32 @@
+<script>
+  import '@tensorflow/tfjs-backend-webgl';
+  import Camera from './lib/Camera.svelte';
+  
+  let video = null;
+  let videoPose = null;
+  let liveStream = null;
+  let livePost = null;
+  let gamePlaying = false;
+
+  const playGame = () => {
+    gamePlaying = true;
+  }
+
+  const stopGame = () => {
+    gamePlaying = false;
+  }
+</script>
+
+<main>
+  <h1>Just Dance - TikTok Edition</h1>
+  <div>
+    <div id="video">
+      
+    </div>
+    <Camera />
+  </div>
+
+</main>
+
+<style>
+</style>
